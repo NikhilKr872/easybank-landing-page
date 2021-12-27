7ab77c60a74e6759ceb895ec184227850bd30eff
Nikhil Kumar <nikhil.kumar.18@mountblue.tech>
2021-12-27 12:02:52 +0530

    adds script to change toggle button

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const toggle=document.querySelector('.dropdown-toggle');
 const navigation=document.querySelector('header .navigation');
+const hamimg=document.querySelector('.ham-img');
+const close_img=document.querySelector('.close-img');
+
 
 toggle.addEventListener('click',()=>{
+    if(close_img.classList.contains('active')){
+        close_img.classList.remove('active')
+    }
+    else{
+        close_img.classList.add('active')
+    }
+    if(hamimg.classList.contains('d')){
+        hamimg.classList.remove('d')
+    }
+    else{
+        hamimg.classList.add('d')
+    }
     if(navigation.classList.contains('active')){
         navigation.classList.remove('active')
     }
@@ -9,3 +24,4 @@ toggle.addEventListener('click',()=>{
         navigation.classList.add('active')
     }
 })
+
